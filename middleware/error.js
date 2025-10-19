bd41c3c5d0a85002da2c3638b9d4521465bbdeeb
Seunvense <90153850+Seunvense@users.noreply.github.com>
2025-10-19 19:21:42 +0100

    Add custom NotFoundError and ValidationError classes

---
+++ middleware/error.js
@@ -0,0 +1,18 @@
+// middleware/error.js
+class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "NotFoundError";
+    this.status = 404;
+  }
+}
+
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ValidationError";
+    this.status = 400;
+  }
+}
+
+module.exports = { NotFoundError, ValidationError };
